647c19e7e04295362bbecad49d5d507add382203
DutraGames <gabrieldutraamarante@gmail.com>
2023-06-03 00:41:08 -0300

    Const ROUTERS in project

---
+++ src/constants/app-routes.ts
@@ -0,0 +1,12 @@
+export const APP_ROUTES = {
+    private:{
+        generate:{
+            name: '/generate'
+        }
+    },
+    public: {
+        home: '/',
+        login: '/login',
+        register: '/register'
+    }
+}
