435b47113cfda5dd7447cee2d2b197a22cf703bb
DutraGames <gabrieldutraamarante@gmail.com>
2023-12-01 11:11:05 -0300

    refactor: altered the layout for new design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,17 +24,10 @@ export default function Login() {
 
     return (
         <main className={styles.main}>
-            <div className={styles.left}>
-                <header onClick={() => router.push('/')} className={styles.header}>
-                    <Image src='GenPass-logo.svg' width={136} height={22} alt='Logo' />
-                </header>
-
-                <h1 className={styles.title}>A segurança dos seus dados é nossa prioridade.</h1>
-
-            </div>
+            <div className={styles.left}></div>
             <div className={styles.right}>
                 <h2 className={styles.subtitle}>Bem-vindo(a) de volta!</h2>
-                <span className={styles.text}>Faça login na sua conta para acessar suas senhas com segurança.</span>
+                <span className={styles.text}>Faça login na sua conta para acessar <br/> suas senhas com segurança.</span>
 
                 <label className={styles.label} htmlFor="email">Seu E-mail</label>
                 <input type="email" name="email" id="email" value={email} onChange={(text) => setEmail(text.target.value)} className={styles.input} placeholder='Campo para inserir o e-mail' />
@@ -42,6 +35,7 @@ export default function Login() {
                 <input type="password" name="senha" value={password} onChange={(text) => setPassword(text.target.value)} id="senha" className={styles.input} placeholder='Campo para inserir a senha' />
                 <button className={styles.btnlogin} onClick={() => handleLogin(email, password)}>Login</button>
                 <button className={styles.btncadastro} onClick={() => router.push('/register')}>Não possui conta? Clique para criar a sua conta</button>
+                <p className={styles.fogot}>Esqueci minha senha, <span onClick={() => router.push('#')}>clique aqui</span></p>
                 <ToastContainer position="bottom-left"
                     autoClose={3000}
                     hideProgressBar={false}
