e9009646039e7385a88b22c36da04d65275e12c5
DutraGames <gabrieldutraamarante@gmail.com>
2023-12-01 09:49:14 -0300

    refactor: modify page index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import styles from './page.module.css'
+import { PlayCircle } from 'iconsax-react'
 
 export default function Home() {
 
@@ -10,19 +11,21 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.left}>
-                <header onClick={() => router.push('/')} className={styles.header}>
-                    <Image src='GenPass-logo.svg' width={136} height={22} alt='Logo' />
-                </header>
+        <header onClick={() => router.push('/')} className={styles.header}>
+          <Image src='GenPass-logo.svg' width={120} height={120} alt='Logo' />
+          <p>GenPass</p>
+        </header>
 
-                <h1 className={styles.title}>Comece a gerenciar suas senhas de forma segura.</h1>
-
-            </div>
-            <div className={styles.right}>
-                <h2 className={styles.subtitle}>Bem-vindo(a)</h2>
-                <span className={styles.text}>Crie uma conta gratuita ou faça login se já tiver uma.</span>
-                <button className={styles.btnlogin} onClick={() => router.push('/login')}>Faça login</button>
-                <button className={styles.btncadastro} onClick={() => router.push('/register')}>Cadastre-se</button>
-            </div>
+        <h1 className={styles.title}>Comece <br /> a gerenciar suas <br /> senhas de <br /> forma segura.</h1>
+      
+        <div className={styles.start} onClick={() => router.push('/login')}>
+          <p>Comece</p>
+          <PlayCircle size={45} color="#000" variant='Bold' />
+        </div>
+      
+      </div>
+      <div className={styles.right}>
+      </div>
     </main>
   )
 }
